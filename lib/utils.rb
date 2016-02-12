f1b1822a56473e3589342e905e2bf02d36b51e05
David P Henderson <dphenderson2@gmail.com>
2016-02-12 13:03:58 -0800

    Change Required Zip Gem to Official Rubyzip
    Change zip gem to official Rubyzip. Change zip open call to correct
    names.

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,5 +1,5 @@
 require 'tempfile'
-require 'zip/zip'
+require 'zip'
 require 'zlib'
 
 module GeoElevation
@@ -25,7 +25,7 @@ module GeoElevation
             temp_file = Tempfile::new(file_name)
             temp_file.write(zip_source)
             temp_file.rewind
-            Zip::ZipFile.open(temp_file) do |zip_file|
+            Zip::File.open(temp_file) do |zip_file|
               zip_file.each do |f|
                 next unless "#{f}" == file_name
                 return f.get_input_stream.read
